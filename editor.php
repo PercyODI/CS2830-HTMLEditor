a172c7f396ce05e3a96f97cb803a251e09c96420
PercyODI <pearse.2008@gmail.com>
2015-09-24 21:41:36 +0000

    Base Commit

---
+++ editor.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    </head>
+    <body>
+        <h1 class="text-center">pah9qd HTML Editor</h1>
+    </body>
+</html>
